434c6b3040a81e9f8ed6185c70cbf4ad3757ee58
Arman Niyaj Mogal <100214997+ArmanMogal@users.noreply.github.com>
2022-05-22 21:15:35 +0530

    Add files via upload
    Quizz app

---
+++ script.js
@@ -0,0 +1,129 @@
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "Which organization defines web standards?",
+        a: "Apple Inc.",
+        b: "IBM Corporation",
+        c: "World wide web Consortium",
+        d: "Microsoft corporation",
+        correct: "c",
+    },
+    {
+        question: "Who is the main author of the HTML?",
+        a: "Brendan Eich",
+        b: "Tim Berners-Lee",
+        c: "Web Developer",
+        d: "Google Inc",
+        correct: "b",
+    },
+    {
+        question: "Javascript is an ___________ language?",
+        a: "Object-Oriented",
+        b: "Object-Based",
+        c: "Procedural",
+        d: "None of the above",
+        correct: "a",
+    },
+    {
+        question: "Which of the following methods can be used to display data in some form using javascript?",
+        a: "document.write()",
+        b: "console.log()",
+        c: "window.alert()",
+        d: "All of the above",
+        correct: "d",
+    },
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
+})
